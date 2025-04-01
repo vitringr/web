240968a3c2b435ff307f5bfbfcd36e2bbc6c1501
Doni <vitringr@pm.me>
2025-04-01 04:08:57 +0300

    refactor: utilities exports

--- utilities/random/src/index.ts
+++ utilities/random/src/index.ts
@@ -1,15 +1,1 @@
-export function bool(): boolean {
-  return Math.random() >= 0.5;
-}
-
-export function percent(percentage: number): boolean {
-  return Math.random() <= percentage * 0.01;
-}
-
-export function range(from: number, to: number): number {
-  return from + Math.random() * (to - from);
-}
-
-export function rangeInt(from: number, to: number): number {
-  return Math.floor(range(from, to));
-}
+export * as Random from "./random"
---
+++ utilities/structures/src/index.ts
@@ -0,0 +1,1 @@
+export * as Structures from "./structures";
