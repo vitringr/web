3eb034a02c13f3039bb32f490782ce7bbe877eb2
Doni <vitringr@pm.me>
2025-04-13 18:10:10 +0300

    refactor(learning-noise): module split

---
+++ packages/learning-noise/src/gradient.ts
@@ -0,0 +1,75 @@
+import { Mathematics } from "@utilities/mathematics";
+import { Canvas2D } from "@utilities/canvas2d";
+import { Vector2 } from "@utilities/vector";
+import { Random } from "@utilities/random";
+import { Config } from "./config";
+
+const pixelsPerCell = Config.pixelsPerRow / Config.cellsPerRow;
+const cellWidth = Config.width / Config.cellsPerRow;
+
+export class Gradient {
+  static createAll() {
+    const gradients: Gradient[][] = [];
+
+    for (let x = 0; x <= Config.cellsPerRow; x++) {
+      gradients.push([]);
+      for (let y = 0; y <= Config.cellsPerRow; y++) {
+        gradients[x].push(new Gradient(x, y));
+      }
+    }
+
+    return gradients;
+  }
+
+  static renderAll(context: CanvasRenderingContext2D, gradients: Gradient[][]) {
+    context.strokeStyle = context.fillStyle = Config.colors.gradient;
+    context.lineWidth = Config.gradientArrowWidth;
+
+    for (const row of gradients) {
+      for (const cell of row) {
+        cell.render(context);
+      }
+    }
+  }
+
+  static getTargetGradients(gradients: Gradient[][], x: number, y: number) {
+    const xCell = Math.floor(x / pixelsPerCell);
+    const yCell = Math.floor(y / pixelsPerCell);
+
+    return [
+      gradients[xCell][yCell],
+      gradients[xCell + 1][yCell],
+      gradients[xCell][yCell + 1],
+      gradients[xCell + 1][yCell + 1],
+    ];
+  }
+
+  readonly vector = Vector2.zero();
+  readonly position = Vector2.zero();
+
+  constructor(
+    readonly x: number,
+    readonly y: number,
+  ) {
+    this.position.set(x * cellWidth, y * cellWidth);
+
+    const angle = Random.range(0, Mathematics.TAU);
+    this.vector.set(Math.cos(angle), Math.sin(angle));
+  }
+
+  render(context: CanvasRenderingContext2D) {
+    Canvas2D.fillCircle(
+      context,
+      this.position.x,
+      this.position.y,
+      Config.gradientCircle,
+    );
+
+    const arrow = this.vector
+      .clone()
+      .scale(Config.gradientArrowLength)
+      .add(this.position);
+    Canvas2D.line(context, this.position.x, this.position.y, arrow.x, arrow.y);
+  }
+}
+
---
+++ packages/learning-noise/src/pixel.ts
@@ -0,0 +1,58 @@
+import { Canvas2D } from "@utilities/canvas2d";
+import { Config } from "./config";
+import { Vector2 } from "@utilities/vector";
+import { Colors } from "@utilities/colors";
+
+const pixelWidth = Config.width / Config.pixelsPerRow;
+
+export class Pixel {
+  static createAll() {
+    const pixels: Pixel[][] = [];
+
+    for (let x = 0; x < Config.pixelsPerRow; x++) {
+      pixels.push([]);
+      for (let y = 0; y < Config.pixelsPerRow; y++) {
+        pixels[x].push(new Pixel(x, y));
+      }
+    }
+
+    return pixels;
+  }
+
+  static renderAll(context: CanvasRenderingContext2D, pixels: Pixel[][]) {
+    for (const row of pixels) {
+      for (const pixel of row) {
+        pixel.render(context);
+      }
+    }
+  }
+
+  static renderPixelLines(context: CanvasRenderingContext2D) {
+    context.strokeStyle = Config.colors.pixelBorder;
+    context.lineWidth = Config.pixelBorderWidth;
+
+    for (let x = 0; x <= Config.pixelsPerRow; x++) {
+      Canvas2D.line(context, 0, x * pixelWidth, Config.width, x * pixelWidth);
+    }
+
+    for (let y = 0; y <= Config.pixelsPerRow; y++) {
+      Canvas2D.line(context, y * pixelWidth, 0, y * pixelWidth, Config.width);
+    }
+  }
+
+  readonly position = Vector2.zero();
+
+  color: number = 0;
+
+  constructor(
+    readonly x: number,
+    readonly y: number,
+  ) {
+    this.position.set(x * pixelWidth, y * pixelWidth);
+  }
+
+  render(context: CanvasRenderingContext2D) {
+    context.fillStyle = Colors.getRGBGrayscale(this.color);
+    context.fillRect(this.position.x, this.position.y, pixelWidth, pixelWidth);
+  }
+}
