ce415331ca70082379045c556d2950ef71dd3537
Doni <vitringr@pm.me>
2025-05-31 03:32:49 +0300

    wip(test)

--- packages/test/src/main.ts
+++ packages/test/src/main.ts
@@ -1,11 +1,10 @@
-import solidPNG from "./assets/solid.png";
-import theSeerPNG from "./assets/theseer.png";
-import hummingBirdPNG from "./assets/hummingbird.png";
+import { Noise } from "@utilities/noise";
+import { pixelData } from "./pixelData";
+import { Mathematics } from "@utilities/mathematics";
 
 const config = {
   width: 640,
   height: 800,
-
 
   imageWidth: 320,
   imageHeight: 400,
@@ -15,11 +14,27 @@ const config = {
   },
 } as const;
 
+type Particle = {
+  active: boolean;
+  x: number;
+  y: number;
+  lifetime: number;
+  angle: number;
+};
+
+type Input = { x: number; y: number; clicked: boolean };
+
+const input: Input = {
+  x: Infinity,
+  y: Infinity,
+  clicked: false,
+};
+
 function setupContext(canvas: HTMLCanvasElement) {
   canvas.width = config.width;
   canvas.height = config.height;
 
-  canvas.style.border = "1px solid red";
+  canvas.style.border = "2px solid gray";
 
   const context = canvas.getContext("2d");
   if (!context) throw "Cannot get 2d context";
@@ -27,44 +42,130 @@ function setupContext(canvas: HTMLCanvasElement) {
   return context;
 }
 
+function setupInput(canvas: HTMLCanvasElement) {
+  const bounds = canvas.getBoundingClientRect();
+  canvas.addEventListener("pointermove", (event: PointerEvent) => {
+    input.x = event.clientX - bounds.left;
+    input.y = event.clientY - bounds.top;
+  });
+
+  canvas.addEventListener("pointerdown", () => {
+    input.clicked = true;
+  });
+
+  window.addEventListener("pointerup", () => {
+    input.clicked = false;
+  });
+
+  window.addEventListener("blur", () => {
+    input.clicked = false;
+  });
+}
+
+function createParticles() {
+  const particles: Particle[] = [];
+
+  for (let i = 0; i < 500; i++) {
+    particles.push({
+      x: Infinity,
+      y: Infinity,
+      active: false,
+      lifetime: 0,
+      angle: Math.random() * Mathematics.TAU,
+    });
+  }
+
+  return particles;
+}
+
 function renderBackground(context: CanvasRenderingContext2D) {
   context.fillStyle = config.colors.background;
   context.fillRect(0, 0, config.width, config.height);
 }
 
+function renderParticle(
+  context: CanvasRenderingContext2D,
+  particle: Particle,
+  color: string,
+) {
+  context.fillStyle = color;
+  const size = particle.lifetime * 0.01;
+  context.fillRect(particle.x, particle.y, size, size);
+}
+
+function updateParticle(particle: Particle) {
+  // WIP
+  particle.x += 1;
+
+  particle.lifetime--;
+  if (particle.lifetime <= 0) {
+    particle.active = false;
+  }
+}
+
 export async function main(canvas: HTMLCanvasElement) {
   const context = setupContext(canvas);
+  setupInput(canvas);
 
-  const img = new Image();
-  img.src = theSeerPNG;
-  img.onload = () => {
-    context.drawImage(img, 0, 0, config.imageWidth, config.imageHeight);
-    const imageData = context.getImageData(
-      0,
-      0,
-      config.imageWidth,
-      config.imageHeight,
-    ).data;
-    context.clearRect(0, 0, config.width, config.height);
+  const particles = createParticles();
 
-    const scale = config.width / config.imageWidth;
-
-    for (let i = 0; i < imageData.length; i += 4) {
-      const r = imageData[i + 0];
-      const g = imageData[i + 1];
-      const b = imageData[i + 2];
-      const a = imageData[i + 3];
-
-      const index = i / 4;
-      const x = (index % config.imageWidth) * scale;
-      const y = Math.floor(index / config.imageWidth) * scale;
-
-      context.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
-      context.fillRect(x, y, scale, scale);
-    }
-  };
+  let spawnIndex = 0;
 
   const loop = () => {
+    if (input.clicked) {
+      console.log("click");
+      spawnIndex++;
+      if (spawnIndex >= 500) spawnIndex = 0;
+
+      const particle = particles[spawnIndex];
+
+      particle.active = true;
+      particle.lifetime = 300;
+      particle.x = input.x;
+      particle.y = input.y;
+    }
+
+    for (let i = 0; i < particles.length; i++) {
+      const particle = particles[i];
+      if (!particle.active) continue;
+
+      particle.lifetime -= 2;
+      if (particle.lifetime <= 0) {
+        particle.active = false;
+      }
+
+      const noise = Noise.get(particle.x * 0.1, particle.y * 0.1) * 2 - 1
+      // particle.angle += particle.angleIncrement * 0.1;
+      particle.angle += noise * 0.2;
+      particle.x += Math.cos(particle.angle);
+      particle.y += Math.sin(particle.angle);
+
+      if (particle.x >= config.width) {
+        particle.active = false;
+        continue;
+      } else if (particle.x <= 0) {
+        particle.active = false;
+        continue;
+      }
+
+      if (particle.y >= config.height) {
+        particle.active = false;
+        continue;
+      } else if (particle.y <= 0) {
+        particle.active = false;
+        continue;
+      }
+
+      const xFloor = Math.floor(particle.x / 2);
+      const yFloor = Math.floor(particle.y / 2);
+
+      context.fillStyle = pixelData[xFloor][yFloor];
+      const size = particle.lifetime * 0.01;
+      context.fillRect(particle.x, particle.y, size, size);
+    }
+
     requestAnimationFrame(loop);
   };
+
+  loop();
 }
