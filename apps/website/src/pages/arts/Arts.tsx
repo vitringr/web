9eb2d1dd7adf6e0d884f052e525348e0c1590772
Doni <vitringr@pm.me>
2025-06-02 03:46:50 +0300

    update(website): arts tags

--- apps/website/src/pages/arts/Arts.tsx
+++ apps/website/src/pages/arts/Arts.tsx
@@ -2,25 +2,25 @@ import { A } from "@solidjs/router";
 
 import css from "./Arts.module.css";
 
-type Tag = {
-  name: string;
-  color: string;
-};
+import { TagNames, TagsContainer } from "./Tags";
 
-// WIP: tags
 const ArtCard = (props: {
   title: string;
   href: string;
   image: string;
-  tags: string[];
+  tags: TagNames[];
 }) => {
   return (
     <div class={css.card}>
       <A class={css.link} href={props.href}>
-        <img class={css.image} src={props.image} />
+        <div class={css.image_container}>
+          <img class={css.image} src={props.image} loading="lazy" />
+          <div class={css.tags_overlay}>
+            <TagsContainer tags={props.tags} />
+          </div>
+        </div>
         <h2 class={css.title}>{props.title}</h2>
       </A>
-      <p>{props.tags.join(" ")}</p>
     </div>
   );
 };
@@ -36,21 +36,53 @@ export const Arts = () => {
           title="The Seer"
           href="/arts/the-seer"
           image="solid.svg"
-          tags={["kek", "aer", "lol"]}
+          tags={[
+            TagNames["2D"],
+            TagNames.Canvas,
+            TagNames.Input,
+            TagNames.Particles,
+            TagNames.Noise,
+          ]}
         />
 
         <ArtCard
           title="Noise2D"
           href="/arts/noise-2d"
           image="solid.svg"
-          tags={["kek", "aer", "lol"]}
+          tags={[
+            TagNames["2D"],
+            TagNames["3D"],
+            TagNames.Canvas,
+            TagNames.Input,
+            TagNames.Particles,
+            TagNames["3D"],
+            TagNames.Canvas,
+            TagNames.GPU,
+            TagNames.Noise,
+            TagNames["2D"],
+            TagNames.Input,
+            TagNames.GPU,
+          ]}
         />
 
         <ArtCard
           title="Noise Loop"
           href="/arts/noise-loop"
           image="solid.svg"
-          tags={["kek", "aer", "lol"]}
+          tags={[
+            TagNames["2D"],
+            TagNames.Input,
+            TagNames["3D"],
+            TagNames.Canvas,
+            TagNames["2D"],
+            TagNames.GPU,
+            TagNames["3D"],
+            TagNames.Canvas,
+            TagNames.GPU,
+            TagNames.Particles,
+            TagNames.Noise,
+            TagNames.Input,
+          ]}
         />
       </div>
     </div>
---
+++ apps/website/src/pages/arts/Tags.tsx
@@ -0,0 +1,89 @@
+import { For } from "solid-js";
+
+import css from "./Tags.module.css";
+
+export enum TagNames {
+  "2D",
+  "3D",
+  Canvas,
+  Input,
+  Noise,
+  GPU,
+  Particles,
+}
+
+type TagData = {
+  label: string;
+  color: string;
+  fontColor: string;
+};
+
+const tags: Record<TagNames, TagData> = {
+  [TagNames["2D"]]: {
+    label: "2D",
+    color: "#118888",
+    fontColor: "#FFFFFF",
+  },
+  [TagNames["3D"]]: {
+    label: "3D",
+    color: "#118888",
+    fontColor: "#FFFFFF",
+  },
+  [TagNames.Canvas]: {
+    label: "Canvas",
+    color: "#FFAA11",
+    fontColor: "#FFFFFF",
+  },
+  [TagNames.Input]: {
+    label: "Input",
+    color: "#11AA11",
+    fontColor: "#FFFFFF",
+  },
+  [TagNames.Noise]: {
+    label: "Noise",
+    color: "#A08070",
+    fontColor: "#FFFFFF",
+  },
+  [TagNames.GPU]: {
+    label: "GPU",
+    color: "#AA1111",
+    fontColor: "#FFFFFF",
+  },
+  [TagNames.Particles]: {
+    label: "Particles",
+    color: "#AA11AA",
+    fontColor: "#FFFFFF",
+  },
+};
+
+const Tag = (props: TagData) => {
+  return (
+    <span
+      class={css.tag}
+      style={{ "background-color": props.color, color: props.fontColor }}
+    >
+      {props.label}
+    </span>
+  );
+};
+
+export const TagsContainer = (props: { tags: TagNames[] }) => {
+  return (
+    <div class={css.tags_container}>
+      <For each={props.tags}>
+        {(tagName) => {
+          const tag = tags[tagName];
+          if (tag) {
+            return (
+              <Tag
+                color={tag.color}
+                fontColor={tag.fontColor}
+                label={tag.label}
+              />
+            );
+          }
+        }}
+      </For>
+    </div>
+  );
+};
