0819411966ecdef149655817d5e5fad37dd18359
Doni <vitringr@pm.me>
2025-06-04 02:39:59 +0300

    add(website): global url links file

---
+++ apps/website/src/links.ts
@@ -0,0 +1,22 @@
+export namespace Links {
+  export const contact = {
+    mail: "vitringr@pm.me",
+    github: "https://github.com/vitringr",
+    twitter: "https://x.com/vitringr",
+  };
+
+  export const honors = {
+    // 1
+    amitPatel: "https://www.redblobgames.com/",
+    danielShiffman: "https://natureofcode.com/",
+    jasperFlick: "https://catlikecoding.com/",
+
+    // 2
+    simonDev: "https://www.youtube.com/@simondev758",
+    thePrimeagen: "https://www.youtube.com/@ThePrimeTimeagen",
+    timRuswick: "https://www.youtube.com/channel/UC_hwKJdF3KRAy4QIaiCSMgQ",
+    lostRelicGames: "https://www.youtube.com/watch?v=159XhSLBHRg",
+    codeMonkey: "https://www.youtube.com/@CodeMonkeyUnity",
+    jasonWeimann: "https://www.youtube.com/@Unity3dCollege",
+  };
+}
--- apps/website/src/pages/contact/Contact.tsx
+++ apps/website/src/pages/contact/Contact.tsx
@@ -1,11 +1,22 @@
-import css from "./Contact.module.css";
+import { Links } from "../../links";
 
 export const Contact = () => {
   return (
     <div class="page">
-
       <h1>Contact</h1>
       <p>Contact page text.</p>
+
+      <div>
+        <a href={Links.contact.mail}>Mail</a>
+      </div>
+
+      <div>
+        <a href={Links.contact.github}>GitHub</a>
+      </div>
+
+      <div>
+        <a href={Links.contact.twitter}>X</a>
+      </div>
     </div>
   );
 };
