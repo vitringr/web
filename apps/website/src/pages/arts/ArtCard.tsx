f99f165ffe0932cad4afcd60da09d23c92ed971a
Doni <vitringr@pm.me>
2025-06-05 13:39:29 +0300

    refactor(website): split artcards component

---
+++ apps/website/src/pages/arts/ArtCard.tsx
@@ -0,0 +1,27 @@
+import { Routes } from "../../routes";
+import { TagNames, TagsContainer } from "./Tags";
+
+import css from "./ArtCard.module.css";
+import { A } from "@solidjs/router";
+
+export const ArtCard = (props: {
+  title: string;
+  route: string;
+  image: string;
+  tags: TagNames[];
+}) => {
+  return (
+    <div class={css.card}>
+      <A class={css.link} href={Routes.root.arts + props.route}>
+        <div class={css.image_container}>
+          <img class={css.image} src={props.image} />
+          <div class={css.tags_overlay}>
+            <TagsContainer tags={props.tags} />
+          </div>
+        </div>
+        <h2 class={css.title}>{props.title}</h2>
+      </A>
+    </div>
+  );
+};
+
--- apps/website/src/pages/arts/Arts.tsx
+++ apps/website/src/pages/arts/Arts.tsx
@@ -1,30 +1,8 @@
-import { A } from "@solidjs/router";
-
 import css from "./Arts.module.css";
 
-import { TagNames, TagsContainer } from "./Tags";
+import { TagNames } from "./Tags";
 import { Routes } from "../../routes";
-
-const ArtCard = (props: {
-  title: string;
-  route: string;
-  image: string;
-  tags: TagNames[];
-}) => {
-  return (
-    <div class={css.card}>
-      <A class={css.link} href={Routes.root.arts + props.route}>
-        <div class={css.image_container}>
-          <img class={css.image} src={props.image} />
-          <div class={css.tags_overlay}>
-            <TagsContainer tags={props.tags} />
-          </div>
-        </div>
-        <h2 class={css.title}>{props.title}</h2>
-      </A>
-    </div>
-  );
-};
+import { ArtCard } from "./ArtCard";
 
 export const Arts = () => {
   return (
