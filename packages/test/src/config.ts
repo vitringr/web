177de03f836bc1c819ff3909c36ba2067c8736ea
Doni <vitringr@pm.me>
2025-06-13 19:51:35 +0300

    cleanup(test)

---
+++ packages/test/src/config.ts
@@ -0,0 +1,8 @@
+export namespace Config {
+  export const width = 600;
+  export const height = 600;
+
+  export const colors = {
+    background: "#111111",
+  } as const;
+}
--- packages/test/src/main.ts
+++ packages/test/src/main.ts
@@ -1,17 +1,8 @@
-import { Canvas2D } from "@utilities/canvas2d";
-
-const config = {
-  width: 600,
-  height: 600,
-
-  colors: {
-    background: "#111111",
-  },
-} as const;
+import { Config } from "./config";
 
 function setupContext(canvas: HTMLCanvasElement) {
-  canvas.width = config.width;
-  canvas.height = config.height;
+  canvas.width = Config.width;
+  canvas.height = Config.height;
 
   canvas.style.border = "1px solid red";
 
@@ -23,30 +14,9 @@ function setupContext(canvas: HTMLCanvasElement) {
 
 export async function main(canvas: HTMLCanvasElement) {
   const context = setupContext(canvas);
-
-  let x = 0;
-  let y = 0;
-  const angle = Math.random() * Math.PI * 2;
-
-  const speed = 5;
-
-  const xVelocity = -Math.cos(angle) * speed;
-  const yVelocity = Math.sin(angle) * speed;
-
-  const radius = 10;
+  console.log(context);
 
   const loop = () => {
-    context.clearRect(0, 0, config.width, config.height);
-
-    context.fillStyle = "white";
-    Canvas2D.circleFill(context, (x += xVelocity), (y += yVelocity), radius);
-
-    if (x <= 0) x = canvas.width;
-    else if (x >= canvas.width) x = 0;
-
-    if (y <= 0) y = canvas.height;
-    else if (y >= canvas.height) y = 0;
-
     requestAnimationFrame(loop);
   };
 
