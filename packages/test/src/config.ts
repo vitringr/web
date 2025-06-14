3dc8645e03bc0225e51d640a209af49dced56195
Doni <vitringr@pm.me>
2025-06-15 00:41:22 +0300

    wip(test)

--- packages/test/src/config.ts
+++ packages/test/src/config.ts
@@ -2,6 +2,9 @@ export namespace Config {
   export const width = 600;
   export const height = 600;
 
+  export const rows = 12;
+  export const cols = 12;
+
   export const colors = {
     background: "#111111",
   } as const;
--- packages/test/src/main.ts
+++ packages/test/src/main.ts
@@ -1,3 +1,4 @@
+import { Canvas2D } from "@utilities/canvas2d";
 import { Config } from "./config";
 
 function setupContext(canvas: HTMLCanvasElement) {
@@ -14,7 +15,21 @@ function setupContext(canvas: HTMLCanvasElement) {
 
 export async function main(canvas: HTMLCanvasElement) {
   const context = setupContext(canvas);
-  console.log(context);
+
+  context.lineWidth = 0.5;
+  context.strokeStyle = "#AAAAAA";
+
+  const xGap = Config.width / Config.cols;
+  for (let x = 0; x < Config.cols; x++) {
+    const step = x * xGap;
+    Canvas2D.line(context, step, 0, step, Config.height);
+  }
+
+  const yGap = Config.height / Config.rows;
+  for (let y = 0; y < Config.rows; y++) {
+    const step = y * yGap;
+    Canvas2D.line(context, 0, step, Config.width, step);
+  }
 
   const loop = () => {
     requestAnimationFrame(loop);
