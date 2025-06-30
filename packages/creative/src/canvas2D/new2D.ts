50800a38ede7a6a5bea99b75026b3e76fd07c6c4
Doni <vitringr@pm.me>
2025-07-01 01:25:39 +0300

    wip(fireworks)

--- packages/creative/src/canvas2D/new2D.ts
+++ packages/creative/src/canvas2D/new2D.ts
@@ -1,37 +1,67 @@
 import { Mathematics } from "@utilities/mathematics";
 
+type PhaseConfig = {
+  spawnCount: number;
+  poolCount: number;
+
+  minSize: number;
+  maxSize: number;
+  decayRate: number;
+
+  speed: number;
+
+  color: string;
+};
+
 const config = {
   width: 800,
   height: 800,
 
-  spawnCount: 10,
-  poolCount: 300,
+  phase1: {
+    spawnCount: 4,
+    poolCount: 400,
 
-  minSize: 8,
-  maxSize: 12,
+    minSize: 6,
+    maxSize: 8,
+    decayRate: 0.06,
 
-  lifetime: 10,
-  decayRate: 0.08,
+    speed: 2,
 
-  speed: 2,
+    color: "teal",
+  } as PhaseConfig,
+
+  phase2: {
+    spawnCount: 10,
+    poolCount: 1000,
+
+    minSize: 8,
+    maxSize: 12,
+    decayRate: 0.08,
+
+    speed: 4,
+
+    color: "#FFAA00",
+  } as PhaseConfig,
 
   timeIncrement: 0.01,
 
-  colors: {
-    background: "#111111",
-  },
+  backgroundColor: "#111111",
 } as const;
 
 const input = { x: -99999, y: -99999, clicked: false };
+let particleIndex1 = 0;
+let particleIndex2 = 0;
 
 type Particle = {
   isAlive: boolean;
-  lifetime: number;
+  toNext: boolean;
+  originalSize: number;
+  size: number;
   x: number;
   y: number;
   xDirection: number;
   yDirection: number;
-  size: number;
+  color: string;
 };
 
 function setupContext(canvas: HTMLCanvasElement) {
@@ -45,7 +75,7 @@ function setupContext(canvas: HTMLCanvasElement) {
 }
 
 function renderBackground(context: CanvasRenderingContext2D) {
-  context.fillStyle = config.colors.background;
+  context.fillStyle = config.backgroundColor;
   context.fillRect(0, 0, config.width, config.height);
 }
 
@@ -61,28 +91,30 @@ function setupInput(canvas: HTMLCanvasElement) {
   });
 }
 
-function createParticlesPool() {
+function createParticlesPool(phaseConfig: PhaseConfig) {
   const particles: Particle[] = [];
 
-  for (let i = 0; i < config.poolCount; i++) {
+  for (let i = 0; i < phaseConfig.poolCount; i++) {
     const angle = Math.random() * Mathematics.TAU;
     const xDirection = Math.cos(angle);
     const yDirection = Math.sin(angle);
 
     const size = Mathematics.lerp(
-      config.minSize,
-      config.maxSize,
+      phaseConfig.minSize,
+      phaseConfig.maxSize,
       Math.random(),
     );
 
     const particle: Particle = {
       isAlive: false,
-      lifetime: 0,
+      toNext: false,
+      originalSize: size,
+      size: size,
       x: 0,
       y: 0,
       xDirection: xDirection,
       yDirection: yDirection,
-      size: size,
+      color: phaseConfig.color,
     };
 
     particles.push(particle);
@@ -91,28 +123,51 @@ function createParticlesPool() {
   return particles;
 }
 
-function moveParticle(particle: Particle) {
-  particle.x += particle.xDirection * config.speed;
-  particle.y += particle.yDirection * config.speed;
+function moveParticle(particle: Particle, phaseConfig: PhaseConfig) {
+  particle.x += particle.xDirection * phaseConfig.speed;
+  particle.y += particle.yDirection * phaseConfig.speed;
 }
 
-function decayParticle(particle: Particle) {
-  particle.lifetime -= config.decayRate;
+function decayParticle(particle: Particle, phaseConfig: PhaseConfig) {
+  particle.size -= phaseConfig.decayRate;
 
-  particle.size = particle.lifetime;
-
-  if (particle.lifetime <= 0) particle.isAlive = false;
+  if (particle.size <= 0) {
+    particle.isAlive = false;
+    particle.toNext = true;
+  }
 }
 
 function renderParticle(particle: Particle, context: CanvasRenderingContext2D) {
+  context.fillStyle = particle.color;
   context.fillRect(particle.x, particle.y, particle.size, particle.size);
 }
 
-function spawnParticle(particle: Particle) {
+function spawnParticleP1(
+  particle: Particle,
+  x: number,
+  y: number,
+  phaseConfig: PhaseConfig,
+) {
   particle.isAlive = true;
-  particle.lifetime = config.lifetime;
-  particle.x = input.x;
-  particle.y = input.y;
+  particle.toNext = false;
+  particle.size = particle.originalSize;
+  particle.x = x;
+  particle.y = y;
+  particle.color = phaseConfig.color;
+}
+
+function spawnParticleP2(
+  particle: Particle,
+  x: number,
+  y: number,
+  phaseConfig: PhaseConfig,
+) {
+  particle.isAlive = true;
+  particle.toNext = false;
+  particle.size = particle.originalSize;
+  particle.x = x;
+  particle.y = y;
+  particle.color = phaseConfig.color;
 }
 
 export function main(canvas: HTMLCanvasElement) {
@@ -120,10 +175,10 @@ export function main(canvas: HTMLCanvasElement) {
 
   const context = setupContext(canvas);
 
-  const particles = createParticlesPool();
+  const particles1 = createParticlesPool(config.phase1);
+  const particles2 = createParticlesPool(config.phase2);
 
   let time = 0;
-  let particleIndex = 0;
   const animation = () => {
     time += config.timeIncrement;
 
@@ -131,21 +186,43 @@ export function main(canvas: HTMLCanvasElement) {
 
     if (input.clicked) {
       input.clicked = false;
-      for (let i = 0; i < config.spawnCount; i++) {
-        const particle = particles[particleIndex];
-        spawnParticle(particle);
-        particleIndex++;
-        particleIndex %= config.poolCount;
+      for (let i = 0; i < config.phase1.spawnCount; i++) {
+        const particle = particles1[particleIndex1];
+        spawnParticleP1(particle, input.x, input.y, config.phase1);
+        particleIndex1++;
+        particleIndex1 %= config.phase1.poolCount;
       }
     }
 
-    context.fillStyle = "orange";
-    for (let i = 0; i < particles.length; i++) {
-      const particle = particles[i];
-      if (!particle.isAlive) continue;
-      moveParticle(particle);
-      renderParticle(particle, context);
-      decayParticle(particle);
+    for (const particle of particles1) {
+      if (particle.isAlive) {
+        moveParticle(particle, config.phase1);
+        renderParticle(particle, context);
+        decayParticle(particle, config.phase1);
+      } else {
+        if (particle.toNext) {
+          particle.toNext = false;
+          for (let i = 0; i < config.phase2.spawnCount; i++) {
+            const newParticle2 = particles2[particleIndex2];
+            spawnParticleP2(
+              newParticle2,
+              particle.x,
+              particle.y,
+              config.phase2,
+            );
+            particleIndex2++;
+            particleIndex2 %= config.phase2.poolCount;
+          }
+        }
+      }
+    }
+
+    for (const particle of particles2) {
+      if (particle.isAlive) {
+        moveParticle(particle, config.phase2);
+        renderParticle(particle, context);
+        decayParticle(particle, config.phase2);
+      }
     }
 
     requestAnimationFrame(animation);
