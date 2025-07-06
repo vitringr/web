0cac4982fe0c3278f20590fda15bc8ab837dcd42
Doni <vitringr@pm.me>
2025-07-06 16:59:18 +0300

    remove(learning-noise): old one

--- packages/learning-noise/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * as LearningNoise from "./main";
--- packages/learning-sdf/src/new/fragment.glsl
+++ packages/learning-sdf/src/new/fragment.glsl
@@ -4,6 +4,20 @@ precision highp float;
 
 out vec4 outColor;
 
+const vec2 CIRCLE_CENTER = vec2(0.5, 0.5);
+const float CIRCLE_RADIUS = 0.2;
+
+const vec4 RED = vec4(1.0, 0.3, 0.4, 1.0);
+const vec4 BLUE = vec4(0.0, 0.3, 0.4, 1.0);
+
 void main() {
-  outColor = vec4(0.0, 0.3, 0.4, 1.0);
+  vec2 point = gl_FragCoord.xy / 600.0;
+
+  outColor = BLUE;
+
+  float distance = distance(point, CIRCLE_CENTER);
+
+  if(distance < CIRCLE_RADIUS) {
+    outColor = RED;
+  }
 }
