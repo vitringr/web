1febad62a31bc99a00bfbf153d3f61d19c00f0c1
Doni <vitringr@pm.me>
2025-07-15 19:16:52 +0300

    architecture(website-arts)

--- apps/website/src/index.tsx
+++ apps/website/src/index.tsx
@@ -7,35 +7,18 @@ import { Routes } from "./routes";
 import { Layout } from "./views/layout/Layout";
 
 import { Home } from "./pages/home/Home";
-
 import { Guides } from "./pages/guides/Guides";
 import { ValueNoise } from "./pages/guides/pages/value-noise/ValueNoise";
 import { SimplexNoise } from "./pages/guides/pages/simplex-noise/SimplexNoise";
 import { PerlinNoise } from "./pages/guides/pages/perlin-noise/PerlinNoise";
-
 import { Arts } from "./pages/arts/Arts";
-import { TheSeer } from "./pages/arts/art-pages/TheSeer";
-import { SystemShock } from "./pages/arts/art-pages/SystemShock";
-import { Anger } from "./pages/arts/art-pages/Anger";
-import { Sparks } from "./pages/arts/art-pages/Sparks";
-import { Overgrowth } from "./pages/arts/art-pages/Overgrowth";
-import { RandomWalkers } from "./pages/arts/art-pages/RandomWalkers";
-import { Noise2D } from "./pages/arts/art-pages/Noise2D";
-import { NoiseFlow } from "./pages/arts/art-pages/NoiseFlow";
-import { NoiseLoop } from "./pages/arts/art-pages/NoiseLoop";
-import { NoiseVectorField } from "./pages/arts/art-pages/NoiseVectorField";
-import { NoiseBlanket } from "./pages/arts/art-pages/NoiseBlanket";
-import { NoiseAscii } from "./pages/arts/art-pages/NoiseAscii";
-import { NoiseRainbow } from "./pages/arts/art-pages/NoiseRainbow";
-import { Regeneration } from "./pages/arts/art-pages/Regeneration";
-import { TenThousand } from "./pages/arts/art-pages/TenThousand";
-import { Layers } from "./pages/arts/art-pages/Layers";
-import { BlockCellularSand } from "./pages/arts/art-pages/BlockCellularSand";
-
 import { Writing } from "./pages/writing/Writing";
 import { About } from "./pages/about/About";
 import { Contact } from "./pages/contact/Contact";
 import { NotFound } from "./pages/not-found/NotFound";
+
+import { artData } from "./pages/arts/art-data/art-data";
+import { ArtPage } from "./pages/arts/ArtPage";
 
 import "./styles/reset.css";
 import "./styles/style.css";
@@ -56,26 +39,10 @@ const AppRouter = () => (
 
     <Route path={Routes.root.arts}>
       <Route path="/" component={Arts} />
-      <Route path={Routes.arts.theSeer} component={TheSeer} />
-      <Route path={Routes.arts.systemShock} component={SystemShock} />
-      <Route path={Routes.arts.anger} component={Anger} />
-      <Route path={Routes.arts.sparks} component={Sparks} />
-      <Route path={Routes.arts.overgrowth} component={Overgrowth} />
-      <Route path={Routes.arts.randomWalkers} component={RandomWalkers} />
-      <Route path={Routes.arts.noise2D} component={Noise2D} />
-      <Route path={Routes.arts.noiseFlow} component={NoiseFlow} />
-      <Route path={Routes.arts.noiseLoop} component={NoiseLoop} />
-      <Route path={Routes.arts.noiseVectorField} component={NoiseVectorField} />
-      <Route path={Routes.arts.noiseBlanket} component={NoiseBlanket} />
-      <Route path={Routes.arts.noiseAscii} component={NoiseAscii} />
-      <Route path={Routes.arts.noiseRainbow} component={NoiseRainbow} />
-      <Route path={Routes.arts.regeneration} component={Regeneration} />
-      <Route path={Routes.arts.tenThousand} component={TenThousand} />
-      <Route path={Routes.arts.layers} component={Layers} />
-      <Route
-        path={Routes.arts.blockCellularSand}
-        component={BlockCellularSand}
-      />
+
+      {artData.map((art) => (
+        <Route path={art.route} component={() => <ArtPage art={art} />} />
+      ))}
     </Route>
 
     <Route path={Routes.root.writing} component={Writing} />
--- apps/website/src/pages/arts/ArtPage.tsx
+++ apps/website/src/pages/arts/ArtPage.tsx
@@ -6,12 +6,16 @@ import css from "./ArtPage.module.css";
 // Additional stuff, like implementation guide and whatnot should be
 // added as Components to the ArtPage props.
 
-export const ArtPage = (props: ArtData<any>) => {
+interface ArtPageProps {
+  art: ArtData<any>;
+}
+
+export const ArtPage = (props: ArtPageProps) => {
   let canvasRef: HTMLCanvasElement | undefined;
 
   onMount(() => {
     if (!canvasRef) throw "Invalid canvasRef";
-    props.artMain(canvasRef, props.artConfig);
+    props.art.artMain(canvasRef, props.art.artConfig);
   });
 
   // TODO: onCleanup
@@ -19,11 +23,11 @@ export const ArtPage = (props: ArtData<any>) => {
   return (
     <main>
       <article>
-        <h1>{props.title}</h1>
+        <h1>{props.art.title}</h1>
 
         <figure>
           <canvas ref={canvasRef} />
-          <figcaption>{props.description}</figcaption>
+          <figcaption>{props.art.description}</figcaption>
         </figure>
       </article>
     </main>
