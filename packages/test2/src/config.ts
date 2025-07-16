39b0ce9e6bfbe8e3f5a3fbbbdca79e9bc96b42ff
Doni <vitringr@pm.me>
2025-07-17 02:01:05 +0300

    wip(test2)

--- packages/test2/src/config.ts
+++ packages/test2/src/config.ts
@@ -2,10 +2,21 @@ export namespace Config {
   export const width = 800;
   export const height = 800;
 
-  export const text = "Hello";
-  export const textSize = 200;
+  export const fieldResolution = 0.4;
+
+  export const text = "Random";
+  export const textSize = 100;
 
   export const colors = {
     background: "#111111",
+    text: [
+      "#FFFFFF",
+      "EEEEEEE",
+      "DDDDDDD",
+      "CCCCCCC",
+      "BBBBBBB",
+      "AAAAAAA",
+    ]
+
   } as const;
 }
