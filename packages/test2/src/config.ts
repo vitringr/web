351d396ca9e749581d597ba10e6aecf2a4f3763e
Doni <vitringr@pm.me>
2025-07-17 02:04:51 +0300

    update

--- packages/test2/src/config.ts
+++ packages/test2/src/config.ts
@@ -5,18 +5,10 @@ export namespace Config {
   export const fieldResolution = 0.4;
 
   export const text = "Random";
-  export const textSize = 100;
+  export const textSize = 150;
 
   export const colors = {
     background: "#111111",
-    text: [
-      "#FFFFFF",
-      "EEEEEEE",
-      "DDDDDDD",
-      "CCCCCCC",
-      "BBBBBBB",
-      "AAAAAAA",
-    ]
-
+    text: ["#FEEBC1", "#FDAF5A", "#FF9F41", "#EE7919", "#D75E03", "#702704"],
   } as const;
 }
