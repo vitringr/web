b5b6bea9e02fc8fb6e61bbab250530b2dd3c53d0
Doni <vitringr@pm.me>
2025-07-21 01:10:10 +0300

    remove(creative): noise flow

--- packages/creative/src/webgl/index.ts
+++ packages/creative/src/webgl/index.ts
@@ -2,7 +2,6 @@ export * as Layers from "./layers/layers";
 export * as TenThousand from "./tenthousand/tenthousand";
 export * as Regeneration from "./regeneration/regeneration";
 export * as Noise2D from "./noise-2d/noise-2d";
-export * as NoiseFlow from "./noise-flow/noise-flow";
 export * as Anger from "./anger/anger";
 export * as Wealth from "./wealth/wealth";
 export * as Stars from "./stars/stars";
