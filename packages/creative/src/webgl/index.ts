de13fb077c5f27b3af73bb3f60f4bd0df2292d6a
Doni <vitringr@pm.me>
2025-07-21 00:49:54 +0300

    remove(creative): godfather

--- packages/creative/src/webgl/index.ts
+++ packages/creative/src/webgl/index.ts
@@ -1,5 +1,4 @@
 export * as BlockCellularAutomata from "./block-cellular-automata/block-cellular-automata";
-export * as Godfather from "./godfather/godfather";
 export * as Layers from "./layers/layers";
 export * as TenThousand from "./tenthousand/tenthousand";
 export * as Regeneration from "./regeneration/regeneration";
