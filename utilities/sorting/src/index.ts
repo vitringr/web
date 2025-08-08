51d91324c2f1c5197edcabe78711a734511e2fc3
Doni <vitringr@pm.me>
2025-08-08 15:19:31 +0300

    add: sorting

---
+++ utilities/sorting/src/index.ts
@@ -0,0 +1,1 @@
+export * as Sorting from "./main";
---
+++ utilities/sorting/src/main.ts
@@ -0,0 +1,55 @@
+/**
+ *
+ * Sorts an array in ascending order.
+ */
+export function bubbleSort(arr: number[]): void {
+  const end = arr.length - 1;
+  for (let i = 0; i < end; i++) {
+    let swapped = false;
+    for (let k = 0; k < end - i; k++) {
+      if (arr[k] > arr[k + 1]) {
+        const temp = arr[k];
+        arr[k] = arr[k + 1];
+        arr[k + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+}
+
+/**
+ *
+ * Sorts an array in ascending order.
+ */
+export function selectionSort(arr: number[]): void {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min = i;
+    for (let k = i + 1; k < arr.length; k++) {
+      if (arr[k] < arr[min]) min = k;
+    }
+    if (min !== i) {
+      const tmp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = tmp;
+    }
+  }
+}
+
+/**
+ *
+ * Sorts an array in ascending order.
+ */
+export function insertionSort(arr: number[]): void {
+  for (let i = 1; i < arr.length; i++) {
+    const current = arr[i];
+    let k = i - 1;
+
+    while (k >= 0 && arr[k] > current) {
+      arr[k + 1] = arr[k];
+      k--;
+    }
+
+    arr[k + 1] = current;
+  }
+}
